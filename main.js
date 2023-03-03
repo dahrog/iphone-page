78dff08953755120ce5e67d43058a754755602b0
dahrog <125228061+dahrog@users.noreply.github.com>
2023-03-03 03:15:15 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let mimg = document.querySelector('.mainimg')
+let container = document.querySelector('.container')
+
+function mphone (mphone){
+    mimg.src = mphone
+}
+
+function bgc (bgc){
+    container.style.background = bgc
+}
